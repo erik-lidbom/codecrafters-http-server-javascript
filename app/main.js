12b8bc49445b4531e1a58eeab7cb28e25b7982d3
eriklidb <eriklidb@student.chalmers.se>
2024-05-24 01:41:30 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,6 +1,10 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
+  socket.on("data", () => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  });
+
   socket.on("close", () => {
     socket.end();
     server.close();
