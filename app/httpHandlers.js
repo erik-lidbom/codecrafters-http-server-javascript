316e8b0fcbc4906aa03c490a3d197834896d6836
eriklidb <eriklidb@student.chalmers.se>
2024-05-24 04:15:16 +0200

    codecrafters submit [skip ci]

--- app/httpHandlers.js
+++ app/httpHandlers.js
@@ -1,26 +1,39 @@
 const httpResponse = (request) => {
-  //Extract the url
+  const headers = request.split("\r\n");
   const url = request.split(" ")[1];
+  const OK_MESSAGE = "HTTP/1.1 200 OK";
+  const NOT_FOUND = "HTTP/1.1 404 Not Found";
 
   if (url === "/") {
-    return "HTTP/1.1 200 OK\r\n\r\n";
+    return `${OK_MESSAGE}\r\n\r\n`;
   } else if (url.includes("/echo/")) {
-    const statusLine = "HTTP/1.1 200 OK";
-    const body = httpBody(url);
+    const body = echoMessage(url);
     const header = httpHeader(body);
-    return `${statusLine}\r\n${header}\r\n${body}`;
+    return `${OK_MESSAGE}\r\n${header}\r\n\r\n${body}`;
+  } else if (url === "/user-agent") {
+    const body = retrieveUserAgent(headers);
+    const header = httpHeader(body);
+
+    return `${OK_MESSAGE}\r\n${header}\r\n\r\n${body}`;
   } else {
-    return "HTTP/1.1 404 Not Found\r\n\r\n";
+    return `${NOT_FOUND}\r\n\r\n`;
   }
 };
 
 const httpHeader = (body) => {
-  return `Content-Type: text/plain\r\nContent-Length: ${body.length}\r\n`;
+  return `Content-Type: text/plain\r\nContent-Length: ${body.length}`;
 };
 
-const httpBody = (url) => {
+const echoMessage = (url) => {
   const body = url.split("/echo/")[1];
   return body;
+};
+
+const retrieveUserAgent = (request) => {
+  const agentValue = request
+    .find((s) => s.startsWith("User-Agent"))
+    .split(" ")[1];
+  return agentValue;
 };
 
 module.exports = { httpResponse };
